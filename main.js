9c17781440570d003e4615fef8a2170e6a84cc63
Andrii Pavliuk <31381969+lv051187paa@users.noreply.github.com>
2023-06-19 08:00:52 +0300

    submit form

--- main.js
+++ main.js
@@ -7,3 +7,21 @@ $('.header-slider').slick({
   pauseOnHover: false,
   speed: 2000
 });
+
+$(".reviews-text-slider").slick({
+  autoplay: true,
+  arrows: false,
+  // dots: true,
+  slidesToShow: 3,
+})
+
+const inputs = document.querySelectorAll("input");
+inputs.forEach((input) => {
+  input.addEventListener("blur", (event) => {
+    if (event.target.value) {
+      input.classList.add("is-valid");
+    } else {
+      input.classList.remove("is-valid");
+    }
+  });
+});
