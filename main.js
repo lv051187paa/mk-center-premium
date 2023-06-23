c7618b21c942e2c78aa788600d61353999d7b441
Andrii Pavliuk <31381969+lv051187paa@users.noreply.github.com>
2023-06-23 12:48:52 +0300

    footer

--- main.js
+++ main.js
@@ -1,27 +1,63 @@
 $('.header-slider').slick({
-  autoplay: true,
-  arrows: false,
-  autoplaySpeed: 4000,
-  fade: true,
-  pauseOnFocus: false,
-  pauseOnHover: false,
-  speed: 2000
+    autoplay: true,
+    arrows: false,
+    autoplaySpeed: 4000,
+    fade: true,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    speed: 2000
 });
 
 $(".reviews-text-slider").slick({
-  autoplay: true,
-  arrows: false,
-  // dots: true,
-  slidesToShow: 3,
+    autoplay: true,
+    arrows: false,
+    // dots: true,
+    slidesToShow: 3,
 })
 
 const inputs = document.querySelectorAll("input");
 inputs.forEach((input) => {
-  input.addEventListener("blur", (event) => {
-    if (event.target.value) {
-      input.classList.add("is-valid");
-    } else {
-      input.classList.remove("is-valid");
-    }
-  });
+    input.addEventListener("blur", (event) => {
+        if (event.target.value) {
+            input.classList.add("is-valid");
+        } else {
+            input.classList.remove("is-valid");
+        }
+    });
 });
+
+
+$('.menu')
+    .addClass('original').clone().insertAfter('.menu')
+    .addClass('cloned')
+    .removeClass('original').hide();
+
+scrollIntervalID = setInterval(stickIt, 10);
+
+
+function stickIt() {
+
+    const orgElement = $('.original');
+    const originalElementPos = orgElement.offset();
+    const originalElementTop = originalElementPos.top;
+
+    if ($(window).scrollTop() > originalElementTop && !$('.cloned').is(":visible")) {
+        const coordsOrgElement = orgElement.offset();
+        const widthOrgElement = orgElement.css('width');
+        const leftOrgElement = coordsOrgElement.left;
+        $('.cloned').css('left',leftOrgElement+'px').css('top',-50).css('width',widthOrgElement).show();
+    }
+
+    if ($(window).scrollTop() > originalElementTop + 60) {
+        // scrolled past the original position; now only show the cloned, sticky element.
+
+        // Cloned element should always have same left position and width as original element.
+
+        $('.cloned').css('top', 0);
+        $('.original').css('visibility','hidden');
+    } else {
+        // not scrolled past the menu; only show the original menu.
+        $('.cloned').hide();
+        $('.original').css('visibility','visible');
+    }
+}
