0e6f2bd4c625ae23fe8f0b38b22bbaf326f4cf9c
Andrii Pavliuk <31381969+lv051187paa@users.noreply.github.com>
2024-01-06 12:20:45 +0200

    access update

--- models/app.sql
+++ models/app.sql
@@ -1,3 +1,3 @@
-CREATE TABLE `mkcenter_premium`.`reviews` (`name` TINYTEXT NOT NULL , `review_text` TINYTEXT NOT NULL , `email` TINYTEXT NOT NULL , `id` INT NOT NULL AUTO_INCREMENT , PRIMARY KEY (`id`)) ENGINE = InnoDB;
+CREATE TABLE `mkcente2_mk_center`.`reviews` (`name` TINYTEXT NOT NULL , `review_text` TINYTEXT NOT NULL , `email` TINYTEXT NOT NULL , `id` INT NOT NULL AUTO_INCREMENT , PRIMARY KEY (`id`)) ENGINE = InnoDB;
 
-ALTER TABLE `mkcenter_premium`.`reviews` ADD `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP AFTER `email`;
+ALTER TABLE `mkcente2_mk_center`.`reviews` ADD `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP AFTER `email`;
